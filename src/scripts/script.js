d16142894d628eff36901b7da8cda132da87459c
riuga <mosks@bk.ru>
2022-07-11 13:00:17 +0700

    fix: reference error

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -3,7 +3,6 @@ const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O"
                     "0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
                     "~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?", "/"];
 
-
 const firstPasswordElement = document.getElementById("first-password");
 const secondPasswordElement = document.getElementById("second-password");
 let isGenerated = false;
